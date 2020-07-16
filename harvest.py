b75a0e8f54ad1d11049e0b378cbe0d4998875834
Iryna Loza <irynloza@gmail.com>
2020-07-16 12:33:06 -0700

    Complete __init__, add_pairing, update_code, make_melon_types functions

--- harvest.py
+++ harvest.py
@@ -11,18 +11,27 @@ class MelonType(object):
         """Initialize a melon."""
 
         self.pairings = []
-
+        
         # Fill in the rest
+        self.code = code
+        self.first_harvest = first_harvest
+        self.color = color
+        self.is_seedless = is_seedless
+        self.is_bestseller = is_bestseller
+        self.name = name
 
     def add_pairing(self, pairing):
         """Add a food pairing to the instance's pairings list."""
 
         # Fill in the rest
+        self.pairings.append(pairing)
+      
 
     def update_code(self, new_code):
         """Replace the reporting code with the new_code."""
 
         # Fill in the rest
+        self.code = new_code
 
 
 def make_melon_types():
@@ -31,6 +40,22 @@ def make_melon_types():
     all_melon_types = []
 
     # Fill in the rest
+    muskmelon = MelonType('musk', 1998, 'green', True, True, 'Muskmelon')
+    muskmelon.add_pairing('mint')
+    all_melon_types.append(muskmelon)
+
+    casaba = MelonType('cas', 2003, 'orange', False, False, 'Casaba')
+    casaba.add_pairing('mint')
+    casaba.add_pairing('strawberries')
+    all_melon_types.append(casaba)
+
+    crenshaw = MelonType('cren', 1996, 'green', False, False, 'Crenshaw')
+    crenshaw.add_pairing('proscuitto')
+    all_melon_types.append(crenshaw)
+
+    yellow_watermelon = MelonType('yw', 2013, 'yellow', False, True, 'Crenshaw')
+    yellow_watermelon.add_pairing('ice cream')
+    all_melon_types.append(yellow_watermelon)
 
     return all_melon_types
 
@@ -38,11 +63,19 @@ def print_pairing_info(melon_types):
     """Prints information about each melon type's pairings."""
 
     # Fill in the rest
+    melon_types = make_melon_types()
+    
+    for melon in melon_types:
+        print(f'{melon.name} pairs with \n - {melon.pairings}')
+    
 
 def make_melon_type_lookup(melon_types):
     """Takes a list of MelonTypes and returns a dictionary of melon type by code."""
 
     # Fill in the rest
+    melon_types = make_melon_types()
+
+
 
 ############
 # Part 2   #
